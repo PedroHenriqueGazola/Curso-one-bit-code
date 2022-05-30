5118fe64bd65c18bedced8b941ddb998f5b78efd
Gazola123 <ph-gazola@hotmail.com>
2022-05-30 15:07:12 -0300

    ex01 Js

---
+++ JS/Aula1/ex01.js
@@ -0,0 +1,5 @@
+alert('Bem vindo, a seguir pediremos que informe alguns dados')
+var nome = prompt('Digite seu nome:')
+var idade = prompt('Digite sua idade:')
+prompt('confime sua idade:')
+alert('O nome digitado foi: ' + nome + ' e sua idade foi: ' + idade)
