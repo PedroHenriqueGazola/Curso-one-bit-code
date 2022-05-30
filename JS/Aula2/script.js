5a744d9f4ceb779566960d0c931a82a16b096853
Gazola123 <ph-gazola@hotmail.com>
2022-05-30 15:29:38 -0300

    ex 02 Js

---
+++ JS/Aula2/script.js
@@ -0,0 +1,7 @@
+var nomevelha = prompt('Qual o nome da pessoa mais velha?')
+var idadevelha = prompt('Qual a idade da pessoa mais velha?')  
+var nomenovo = prompt('Qual o nome da pessoa mais nova?')
+var idadenovo = prompt('Qual a idade da pessoa mais nova?')
+alert('O nome da pessoa mais velha é: ' + nomevelha + ' e sua idade é: ' + idadevelha)
+alert('O nome da pessoa mais nova é: ' + nomenovo + ' e sua idade é: ' + idadenovo)
+alert('A diferença de idade entre ' + nomevelha + ' e '+ nomenovo + ' é: ' + (idadevelha - idadenovo))
