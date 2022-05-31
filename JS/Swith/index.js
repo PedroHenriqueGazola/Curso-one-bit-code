ae05297bd4fd179b35e81c99e9003446b3cb1e20
Gazola123 <ph-gazola@hotmail.com>
2022-05-31 20:58:40 -0300

    swithJs

---
+++ JS/Swith/index.js
@@ -0,0 +1,15 @@
+let velocity = 80;
+
+switch(velocity) {
+    case 80:
+        console.log("Sua velocidade é de 80km/h");
+        break;
+    case 160:
+        console.log("Sua velocidade é de 160km/h");
+        break;
+    default: 
+        console.log("Velocidade não indentificada");
+        break;
+}
+
+//Avalia a condiçao e retorna o valor true ou false//
