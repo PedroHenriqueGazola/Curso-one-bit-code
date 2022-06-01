788404681c444b0fdfec4c467192593b2d7f026b
Gazola123 <ph-gazola@hotmail.com>
2022-06-01 13:39:25 -0300

    AulaFunction1

---
+++ JS/AulaFunction/index.js
@@ -0,0 +1,9 @@
+function speedUp(velocity, acceleration) {
+    let newVelocity = velocity + acceleration;
+    console.log('Velocidade atual: ' + velocity + '\nAceleração: ' + acceleration + '\nVelocidade atualizada: ' + newVelocity);
+}
+
+speedUp(10, 5);
+speedUp(10, 10);
+speedUp(10, 15);
+speedUp(10, 20);
