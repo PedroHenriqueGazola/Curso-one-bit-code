719b15007ce5c6cec46e6f6b9830bcdd8ff2181b
Gazola123 <ph-gazola@hotmail.com>
2022-06-01 11:38:55 -0300

    AulaDoWhileJs

---
+++ JS/AulaDoWhile/index.js
@@ -0,0 +1,9 @@
+let velocity = 100
+let acceleration = 5
+
+do {
+    velocity += acceleration
+    console.log(velocity)
+} while (velocity < 200)
+
+// A expressao so é avaliada dps de ser executada 1 vez, diferindo do while q é avaliada antes de ser executada//
