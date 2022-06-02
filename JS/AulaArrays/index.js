3f68730d2d3d18712c56e6183caefed501bf14c5
Gazola123 <ph-gazola@hotmail.com>
2022-06-02 18:06:25 -0300

    array

---
+++ JS/AulaArrays/index.js
@@ -0,0 +1,34 @@
+/*var Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(Numbers);
+console.log(Numbers[1]);*/
+
+//Armazena varios valores em uma unica variavel//
+
+//Operaçoes com arrays//
+
+var Numbers = [1, 2, 3, 4];
+
+//Numbers.push(5); //Adiciona o valor 5 no final do array//
+
+//Numbers.pop(); //Remove o ultimo valor do array//
+
+//var removedItem = Numbers.pop(); //Remove o ultimo valor do array e armazena em removedItem//
+
+//Numbers.shift(); //Remove o primeiro valor do array//
+
+//var removedItem = Numbers.shift(); //Remove o primeiro valor do array e armazena em removedItem//
+
+//Numbers.unshift(0); //Adiciona o valor 0 no inicio do array//
+
+//console.log(Numbers.length); //Retorna o tamanho do array//
+
+console.log(Numbers);
+
+var elementalPositions = Numbers.indexOf(7); //Retorna a posição do valor 3 no array, caso ele n encontre o elemento, retorna a posiçao -1//
+
+console.log(elementalPositions);
+
+
+
+
---
+++ JS/AulaArrays/index2.js
@@ -0,0 +1,20 @@
+var lista = [["elemental", 7], ["helmet", 6],["golias", 5],["supernova", 6]]
+
+console.log(lista[3][0]); // Para acessar uma array dentro de outra array//
+
+
+let spaceshipsList = ["Colossus", "Helmet", ["Fenix", "Revivor"], "Supernova"]
+
+spaceshipsList.pop()
+
+spaceshipsList.pop()
+
+spaceshipsList.push("Elemental")
+
+spaceshipsList.unshift("Eagle")
+
+spaceshipsList.shift()
+
+spaceshipsList.unshift("")
+
+console.log(spaceshipsList);
