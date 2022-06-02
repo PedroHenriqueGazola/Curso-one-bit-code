2b1df0512e7882d35ca332a83bb60770e3b713bf
Gazola123 <ph-gazola@hotmail.com>
2022-06-02 11:52:15 -0300

    functions modelos

---
+++ JS/AulaFunction/ArrowFunction.js
@@ -0,0 +1,3 @@
+const doubleSpeed = speed => speed * 2;
+// uma maneira muito simplificada de declarar uma função anonima é usando o operador flecha (=>).
+console.log(doubleSpeed(10));
---
+++ JS/AulaFunction/FunçoesAnonimas.js
@@ -0,0 +1,7 @@
+let doubleSpeed = function(speed) {
+    return speed * 2;
+}
+
+console.log(doubleSpeed(10));
+
+// as funçoes anonimas, diferente das funçoes normais, so podem ser chamadas depois de declaradas.//
