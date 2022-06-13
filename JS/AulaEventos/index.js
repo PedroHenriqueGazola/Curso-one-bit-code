b76b76b5a7ee52f4f7f9c2e6d149b88b58513d1d
Gazola123 <ph-gazola@hotmail.com>
2022-06-13 20:12:37 -0300

    Eventos no HTML com JS

---
+++ JS/AulaEventos/index.js
@@ -0,0 +1,15 @@
+function clickMe() {
+    console.log("clicou")
+}
+function focusMe() {
+    console.log("Focado")
+}
+function blurMe() {
+    console.log("desfocado")
+}
+function changeMe() {
+    console.log("alterado")
+}
+function overMe() {
+    console.log("Mouse aqui")
+}
