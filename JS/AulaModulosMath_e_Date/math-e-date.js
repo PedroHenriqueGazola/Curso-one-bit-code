a51f98dd2ef9fd407337233cb51cc2808ce22ae2
Gazola123 <ph-gazola@hotmail.com>
2022-06-13 20:00:32 -0300

    Math e Date Js

---
+++ JS/AulaModulosMath_e_Date/math-e-date.js
@@ -0,0 +1,15 @@
+//Math
+
+console.log(Math.pow(5,3)) // 5 elevado a 3 = 125
+console.log(Math.sqrt(25)) // raiz quadrada de 25 = 5
+console.log(Math.PI) // pi = 3.141592653589793...
+//  O modulo Math possui diversos metodos para trabalhar com numeros e datas. É uma classe nativa do JavaScript.//
+
+//Date
+
+let myBirthday = new Date(2004, 07, 23) // o primeiro parametro é o ano, o segundo o mes, o terceiro o dia.
+console.log(myBirthday) // 23/08/2004, o primeiro mes para o Date é o 0, ou seja, Janeiro.
+
+let today = Date.now() // pega a data atual.
+console.log(today) // 159841008984 milisegundos desde 1/1/1970.
+// O modulo Date possui diversos metodos para trabalhar com datas. É uma classe nativa do JavaScript.//
