36b5b05dbb3ee896f0d92dce42227c20e01bab2c
Gazola123 <ph-gazola@hotmail.com>
2022-06-19 17:01:53 -0300

    refazendo projeto

--- JS/Compra de anão/JS/index.js
+++ JS/Compra de anão/JS/index.js
@@ -18,23 +18,24 @@ function cadastro() {
     removeButton.type = "button"; // seta o tipo do button como button
     removeButton.setAttribute("onclick", "remover(this)"); // seta o evento onclick do button, e passa a funcao remover() como parametro
     newList.appendChild(removeButton); // adiciona o button ao li
-
+    
     var buyButton = document.createElement("button"); // cria um elemento button
     buyButton.innerText = "Comprar"; // seta o texto do button com o valor comprar
     buyButton.type = "button"; // seta o tipo do button como button
     buyButton.setAttribute("onclick", "buyButtonFunction(this)"); // seta o evento onclick do button, e passa a funcao buyButtonFunction() como parametro
     newList.appendChild(buyButton); // adiciona o button ao li*/
-}
 
+    document.getElementById("res").appendChild(newList); // adiciona o elemento li na div com id res
+
+}
 function remover(button) {
     var liToRemove = button.parentNode; // selecionar o Elemento pai do button, que é o li
     document.getElementById("res").removeChild(liToRemove); // remove o li da lista de a venda  
 }
-
 function buyButtonFunction(button) {
     var liTobuy = button.parentNode; // selecionar o Elemento pai do button, que é o li
     var buylist = document.getElementById("buyList") // seleciona o elemento com id buyList
     buylist.appendChild(liTobuy); // adiciona o li a lista de comprado
     liTobuy.removeChild(button); // remove o button comprar do li
     liTobuy.removeChild(liTobuy.lastChild); // remove o button remover do li
-}
+}   
