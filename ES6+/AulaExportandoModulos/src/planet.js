c3ce5438a98182ff47a3279509b1d056c62e8e88
Pedro Gazola <ph-gazola@hotmail.com>
2022-06-28 21:29:24 -0300

    Usando e exportando modulos

---
+++ ES6+/AulaExportandoModulos/src/planet.js
@@ -0,0 +1,12 @@
+class planet {
+    constructor(name, surfaceArea) {
+        this.name = name;
+        this.surfaceArea = surfaceArea;
+    }
+
+    rotate() {
+        console.log('rotacionando')
+    }
+}
+
+module.exports = planet;
