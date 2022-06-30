530de1eb3bf6071a5eee86ce2ce0406e35f84262
Pedro Gazola <ph-gazola@hotmail.com>
2022-06-30 19:47:10 -0300

    Renomeando Imports e Exports

--- ES6+/AulaExport_e_Import_Defaut/src/defense.js
+++ ES6+/AulaExport_e_Import_Defaut/src/defense.js
@@ -1,4 +1,4 @@
-export const shieldDefenses = ["Escudo de Metal", "Escudo de Vidro"]
-export const armorDefenses = ["Armadura de Metal", "Armadura de Vidro"]
+const shieldDefenses = ["Escudo de Metal", " Escudo de Vidro"]
+const armorDefenses = ["Armadura de Metal", " Armadura de Vidro"]
 
-//export {shieldDefenses, armorDefenses}
+export {shieldDefenses as shieldDef, armorDefenses as armorDef} // rename the export to shieldDefenses
--- ES6+/AulaExport_e_Import_Defaut/src/index.js
+++ ES6+/AulaExport_e_Import_Defaut/src/index.js
@@ -1,9 +1,9 @@
 import Spaceship from "./spaceship";
 import armamentKind from "./armament";
-import {shieldDefenses} from "./defense";
+import {shieldDef as shieldD, armorDef} from "./defense"; // rename the import to shieldDef
 
-const spaceship = new Spaceship("USS Enterprice", "James", armamentKind.laser, shieldDefenses);
+const spaceship = new Spaceship("USS Enterprice", "James", armamentKind.laser, shieldD);
 
-var message = `Nome da nave: ${spaceship.name}, Capitão: ${spaceship.captain}, Armamentos: ${spaceship.armaments}, Defesas: ${spaceship.defenses}`;
+var message = `Nome da nave: ${spaceship.name}, Capitão: ${spaceship.captain}, Armamentos: ${spaceship.armaments}, Defesas: ${spaceship.defenses + ", " + armorDef}`;
 
 console.log(message);
