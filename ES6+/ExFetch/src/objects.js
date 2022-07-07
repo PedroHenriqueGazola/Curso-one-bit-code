e8550a822b0c52bbfc93423e7613cf0850a02ff0
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-07 12:30:52 -0300

    Exercicio Fetch

---
+++ ES6+/ExFetch/src/objects.js
@@ -0,0 +1,8 @@
+export default class Objects {
+    constructor(id, name, sentryyearmin, sentryyearmax) {
+        this.id = id
+        this.name = name
+        this.sentryyearmin = sentryyearmin
+        this.sentryyearmax = sentryyearmax
+    }
+}
