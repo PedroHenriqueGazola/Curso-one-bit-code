d5712dd24dbf7d3726555384529002ec6f962597
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-08 12:29:45 -0300

    Dividindo e reutilizando components

--- React/excriandocomponents/src/components/planets/planet/index.js
+++ React/excriandocomponents/src/components/planets/planet/index.js
@@ -1,11 +1,11 @@
-import './style.css';
+import GrayImg from "../../shared/gray_image";
 
 const Planet = () => {
     return (
         <div>
             <h4>Mercury</h4>
             <p>legenda</p>
-            <img class = "img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1024px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="" />
+            <GrayImg />
         </div>
     );
 }
---
+++ React/excriandocomponents/src/components/shared/gray_image/index.js
@@ -0,0 +1,11 @@
+import  './style.css';
+
+const GrayImg = () => {
+    return (
+        <div className="gray-image">
+            <img class= "img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1024px-Mercury_in_color_-_Prockter07-edit1.jpg" alt=""></img>
+        </div>
+    );
+}
+
+export default GrayImg;
