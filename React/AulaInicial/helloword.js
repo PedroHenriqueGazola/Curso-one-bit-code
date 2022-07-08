1de7976f63bb4bc29e82621055d02eaf66509cc7
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-07 22:19:19 -0300

    Aula inicial react

---
+++ React/AulaInicial/helloword.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+    React.createElement( 
+        'h1', // tag
+        null, // props(ID, class, style, etc)
+        'Hello World', // conteudo
+    ),
+    document.getElementById('root') // elemento onde será renderizado
+)
