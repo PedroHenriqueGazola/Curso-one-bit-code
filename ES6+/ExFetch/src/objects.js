ca768839239dc128ecff0ff226baf7309bffe01a
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-11 02:26:25 -0300

    Alteraçao moment.js

--- ES6+/ExFetch/src/objects.js
+++
@@ -1,8 +0,0 @@
-export default class Objects {
-    constructor(id, name, sentryyearmin, sentryyearmax) {
-        this.id = id
-        this.name = name
-        this.sentryyearmin = sentryyearmin
-        this.sentryyearmax = sentryyearmax
-    }
-}
