b5c0e0cc4f2564d05cf8f9ac7652c2299907a305
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-22 23:55:16 -0300

    Alteraçoes no Planets/Form

--- React/excriandocomponents/src/components/planets/form/index.js
+++ React/excriandocomponents/src/components/planets/form/index.js
@@ -12,18 +12,18 @@ const Form = props => {
 
     const handleFieldsChange = e => setfields({
         ...fields,
-        [e.target.name]: e.target.value
+        [e.currentTarget.name]: e.currentTarget.value
     });
 
     const handleSubmit = event => {
-        event.preventDefault();
         props.addPlanet(fields);
+        event.preventDefault();
         setfields(initialState);
     }
 
     return (
     <Fragment>
-        <form>
+        <form onSubmit={handleSubmit}> 
             <div>
                 <label htmlFor="name">Name: </label>
                 <input type="text" id="name" name="name" value={fields.name} onChange = {handleFieldsChange}/>
@@ -40,7 +40,7 @@ const Form = props => {
                 <label htmlFor="img_url">Imagem: </label>
                 <input type="text" id="img_url" name="img_url" value={fields.img_url} onChange = {handleFieldsChange}/>
             </div>
-            <input type={"submit"} onClick = {handleSubmit}/>
+            <input type={"submit"}/>
 
         </form>
 
