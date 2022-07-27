13da61515075faa93224e0e9813b98052b105b10
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-27 20:05:56 -0300

    Tipos extras com Typescript

---
+++ Typescript/AulaTiposExtras/enums.ts
@@ -0,0 +1,14 @@
+enum Planets {
+    Mercury,
+    Venus,
+    Earth 
+}   // enum é um tipo especial de dados que pode conter apenas um conjunto de valores, transforma a string em um numero
+
+Planets.Earth
+
+enum Roles {
+    ADMIN,
+    USER
+}
+
+Roles.ADMIN 
---
+++ Typescript/AulaTiposExtras/tuplas.ts
@@ -0,0 +1,5 @@
+let crew: [string, string, number]; // Declaração de uma tupla, limita o array a ser exatamente 3 posições, com os tipos especificados
+
+crew = ['José', 'Maria', 20]
+
+console.log(crew)
